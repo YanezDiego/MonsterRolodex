de102411571c3e871ac130a4c0fe110d571aadfc
Diego Yanez <diegosyanez92@gmail.com>
2022-10-27 14:13:48 -0400

    add user fetch when component mount and display

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {Component} from 'react';
 
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -11,10 +11,26 @@ class App extends Component {
     };
   };
 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(() => {
+      return {monsters: users}
+    }, () => console.log(this.state) //loggin to make sure state has been updated on a real application we would not do this
+    ));
+  };
+
   render(){
     return (
       <div className="App">
-        
+        {this.state.monsters.map((monster) => {
+          return(
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+          })
+        }
       </div>
     );
   };
