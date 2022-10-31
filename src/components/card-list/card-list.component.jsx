7e4870d1e24fa7d1404f8f8d63aeeb7beb0b2e1c
Diego Yanez <diegosyanez92@gmail.com>
2022-10-31 14:56:29 -0400

    scafolded card list component

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class CardList extends Component {
+
+
+    render(){
+        return(
+            <div>Card List goes Here</div>
+        );
+    };
+};
+
+export default CardList;
